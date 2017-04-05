084725ca04ec34e1929db3e83000d6eabf15bce2
Danny Kirschner <rkirschner1377@gmail.com>
2017-04-05 17:29:50 -0400

    Adding solution files
    Adding comment about linter in solution code for `reduce`
    Rubocop wants `each_with_object` to remove the explicit return of `memo`
    
    Refresh for cohort lm01

--- bin/range_lab.rb
+++ bin/range_lab.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
 # range as enumerable lab
+
+range = (1..10)
+
+puts "sum all #{range.reduce(:+)}"
+
+puts "sum even #{range.select(&:even?).reduce(:+)}"
+
+puts "sum odd #{range.select(&:odd?).reduce(:+)}"
+
+# why might this method be preferable? Hint: count number of iterations
+altogether = range.each_with_object(Hash.new(0)) do |range_element, hash|
+  hash[:even] += range_element if range_element.even?
+  hash[:odd] += range_element if range_element.odd?
+  hash[:all] += range_element
+end
+
+puts "sum everything #{altogether}"
